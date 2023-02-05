9ce3bfc412dca1ac2c137355264e540ddb274be8
Ismail Ali <Ismailali027@gmail.com>
2023-02-04 22:00:56 -0600

    question #7 first piece of logic is working

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -60,6 +60,12 @@ for( let i=0; i<supplyChanges.length; i++) {
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
+for( items of supplyChanges){
+    console.log('for of loop', items);
+    if(items > 0){
+        console.log('Added x parts');
+    }
+}
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
